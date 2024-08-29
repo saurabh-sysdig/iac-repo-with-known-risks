07ec51ccab4aca3758b6775d15ea3e895a88172f
saurabh-sysdig <saurabh.bhore@sysdig.com>
2024-08-30 01:36:51 +0530

    Add IaC files with known risks

---
+++ aws_security_group.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group" "example" {
+  name = "example"
+  // Risk: open security group allowing all inbound traffic
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
